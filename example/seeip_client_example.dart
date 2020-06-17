c185a06117669f5a6d851ba7728c6ea31fc05624
mab <madaboutbrighton@users.noreply.github.com>
2020-06-17 21:42:27 +0100

    Omit local variable types

--- example/seeip_client_example.dart
+++ example/seeip_client_example.dart
@@ -4,26 +4,22 @@
 
 import 'package:seeip_client/seeip_client.dart';
 
-main() async {
-  SeeipClient seeip = SeeipClient();
+void main() async {
+  var seeip = SeeipClient();
 
   // Fetches only the IP address of the requesting device.
-  OnlyIP ip = await seeip.getIP();
-
+  var ip = await seeip.getIP();
   print(ip);
 
   // Fetches only the IPv4 address of the requesting device.
-  OnlyIP ipv4 = await seeip.getIPv4();
-
+  var ipv4 = await seeip.getIPv4();
   print(ipv4);
 
   // Fetches the Geo IP of the requesting device.
-  GeoIP geoIP = await seeip.getGeoIP();
-
+  var geoIP = await seeip.getGeoIP();
   print(geoIP);
 
   // Fetches the Geo IP of the specified IP address.
-  GeoIP geoIPSpecific = await seeip.getGeoIP('216.58.208.174');
-
+  var geoIPSpecific = await seeip.getGeoIP('216.58.208.174');
   print(geoIPSpecific);
 }
