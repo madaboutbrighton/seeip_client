067ea1b8947799c9cc5da065ad639cc403bde9cc
madaboutbrighton <west@madaboutbrighton.net>
2023-10-26 16:40:41 +0100

    Removed header comments.

--- example/seeip_client_example.dart
+++ example/seeip_client_example.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2019, Mad About Brighton. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:seeip_client/seeip_client.dart';
 
 void main() async {
